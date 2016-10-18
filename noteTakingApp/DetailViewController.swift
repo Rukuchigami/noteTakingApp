86d1faffeaea0ce4416d389540cc77f1d73a34a3
Rukuchigami <aparker113088@gmail.com>
2016-10-18 17:48:29 -0400

    Finished note taking app

--- noteTakingApp/DetailViewController.swift
+++ noteTakingApp/DetailViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UITextViewDelegate {
 
     @IBOutlet weak var detailDescriptionLabel: UITextView!
 
@@ -38,13 +38,36 @@ class DetailViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         detailViewController = self
         detailDescriptionLabel.becomeFirstResponder()
+        detailDescriptionLabel.delegate = self
         self.configureView()
     }//end of ViewDidLoad
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }// end of didRevieceMemoryWarning
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if objects.count == 0{
+            return
+        }
+        objects[currentIndex] = detailDescriptionLabel.text
+        if detailDescriptionLabel.text == ""{
+         objects[currentIndex] = BLANK_NOTE
+        }
+        saveAndUpdte()
     }
+    
+    func saveAndUpdte(){
+        masterView?.save()
+        masterView?.tableView.reloadData()
+    }//end of saveAndUpdate
+    
+    func textViewDidChange(_ textView: UITextView) {
+        objects[currentIndex] = detailDescriptionLabel.text
+        saveAndUpdte()
+    }//end of text view did change
 
 
 }//end of UIView Controller
--- noteTakingApp/MasterViewController.swift
+++ noteTakingApp/MasterViewController.swift
@@ -21,6 +21,7 @@ class MasterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        masterView = self
         load()
         self.navigationItem.leftBarButtonItem = self.editButtonItem
 
@@ -33,6 +34,13 @@ class MasterViewController: UITableViewController {
         save()
         super.viewWillAppear(animated)
     }//end of view WillAppear
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if objects.count == 0 {
+            insertNewObject(self)
+        }
+        super.viewDidAppear(animated)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -40,22 +48,31 @@ class MasterViewController: UITableViewController {
     }//end of didReviceMemoryWarning
 
     func insertNewObject(_ sender: AnyObject) {
-        objects.insert(BLANK_NOTE, at: 0)
-        let indexPath = IndexPath(row: 0, section: 0)
-        self.tableView.insertRows(at: [indexPath], with: .automatic)
+        if detailViewController?.detailDescriptionLabel.isEditable == false {
+            return
+        }
+        if objects.count == 0 || objects[0] != BLANK_NOTE {
+            objects.insert(BLANK_NOTE, at: 0)
+            let indexPath = IndexPath(row: 0, section: 0)
+            self.tableView.insertRows(at: [indexPath], with: .automatic)
+        }//end of objects.count if statement
+        currentIndex = 0
+        self.performSegue(withIdentifier: "showDetail", sender: self)
     }//end of insert new object
 
     // MARK: - Segues
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        detailViewController?.detailDescriptionLabel.isEditable = true
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[(indexPath as NSIndexPath).row]
                 currentIndex = (indexPath as NSIndexPath).row
-                detailViewController?.detailItem = object as AnyObject?
-                detailViewController?.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem
-                detailViewController?.navigationItem.leftItemsSupplementBackButton = true
-            }
+                }
+            let object = objects [currentIndex]
+            detailViewController?.detailItem = object as AnyObject?
+            detailViewController?.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem
+            detailViewController?.navigationItem.leftItemsSupplementBackButton = true
+            
         }
     }//end of UIStoryboardSeque
 
@@ -90,9 +107,25 @@ class MasterViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
     }//end of func table view
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+         super.setEditing(editing, animated: animated)
+        if editing{
+            detailViewController?.detailDescriptionLabel.isEditable = false
+            detailViewController?.detailDescriptionLabel.text = " "
+            return
+        }
+        save()
+    }//end of set editing function
+    
+    override func tableView(_ tableView: UITableView, didEndEditingRowAt indexPath: IndexPath?) {
+        detailViewController?.detailDescriptionLabel.isEditable = false
+        detailViewController?.detailDescriptionLabel.text = " "
+        save()
+    }
 
     func save(){
-        UserDefaults.standard.object(forKey: kNotes)
+        UserDefaults.standard.set(objects, forKey: kNotes)
         UserDefaults.standard.synchronize()
     }//end of save function
     
